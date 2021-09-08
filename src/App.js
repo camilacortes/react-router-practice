c567304c98a3d49b4097ab980884ab9106bbea3a
unknown <camilacortes0103@gmail.com>
2021-09-08 17:50:55 -0600

    setup links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from '../src/components/Home'
+import About from '../src/components/About'
+import Services from '../src/components/Services'
+
+import {Switch, Route, Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <nav className="Navigation">
+      <Link to="/"><Home/></Link>
+      <Link to="/About"><About/></Link>
+      <Link to="/Services"><Services/></Link>
+    </nav>
     </div>
   );
 }
